fea9526f106cda09e4fb6c5b64a78c2827ca924d
YCtee <104292374+YCtee@users.noreply.github.com>
2020-02-29 17:09:45 +0000

    Bump version to 0.2.5

--- eslint-prettier-config.sh
+++ eslint-prettier-config.sh
@@ -35,4 +35,4 @@ echo -e "${LCYAN}Which package manager are you using? ${NC}"
 select config_extension in ".js" ".json" "Cancel"; do
   case $config_extension in
     .js ) config_opening='module.exports = {'; break;;
-    .json ) config_opening='{'
+    .json ) config_opening='{'; break;
