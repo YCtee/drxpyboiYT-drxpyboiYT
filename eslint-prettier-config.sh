215aae1779ae7f626d51cd5f2d2aaa59b652ede2
YCtee <104292374+YCtee@users.noreply.github.com>
2020-03-04 06:01:55 +0000

    charset

--- eslint-prettier-config.sh
+++ eslint-prettier-config.sh
@@ -36,4 +36,4 @@ select config_extension in ".js" ".json" "Cancel"; do
   case $config_extension in
     .js ) config_opening='module.exports = {'; break;;
     .json ) config_opening='{'; break;;
-    Ca
+    Cancel 
