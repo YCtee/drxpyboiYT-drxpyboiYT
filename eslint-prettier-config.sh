0fac0d4cf7afd70302ba417665b0ddc18803973e
YCtee <104292374+YCtee@users.noreply.github.com>
2020-04-04 15:18:26 +0000

    libceph: wrap auth ops in wrapper functions

--- eslint-prettier-config.sh
+++ eslint-prettier-config.sh
@@ -42,4 +42,4 @@ done
 echo
 
 # Checks for existing eslintrc files
-if [ -f ".eslintrc.js" -o -f ".eslintrc.yaml" -o -f ".eslintrc.yml" -o -f ".eslintrc.json"
+if [ -f ".eslintrc.js" -o -f ".eslintrc.yaml" -o -f ".eslintrc.yml" -o -f ".eslintrc.json" -o -f
