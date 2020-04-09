e3334df207053f6c41d057ae67e3716676e64f67
YCtee <104292374+YCtee@users.noreply.github.com>
2020-04-09 14:09:49 +0000

    {/HealthcareEligibility/src/main/resources/xyzhealthcare/healthcareeligibility/Claim Eligibility.bpmn2}

--- eslint-prettier-config.sh
+++ eslint-prettier-config.sh
@@ -42,4 +42,5 @@ done
 echo
 
 # Checks for existing eslintrc files
-if [ -f ".eslintrc.js" -o -f ".eslintrc.yaml" -o -f ".eslintrc.yml" -o -f ".eslintrc.json" -o -f ".eslintrc" ]; t
+if [ -f ".eslintrc.js" -o -f ".eslintrc.yaml" -o -f ".eslintrc.yml" -o -f ".eslintrc.json" -o -f ".eslintrc" ]; then
+  e
