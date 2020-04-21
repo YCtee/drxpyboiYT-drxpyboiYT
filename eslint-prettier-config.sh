6e4721f9a77c2a80c04e704893d2dd230d952c37
YCtee <104292374+YCtee@users.noreply.github.com>
2020-04-21 16:13:53 +0000

    updated sdk

--- eslint-prettier-config.sh
+++ eslint-prettier-config.sh
@@ -43,4 +43,4 @@ echo
 
 # Checks for existing eslintrc files
 if [ -f ".eslintrc.js" -o -f ".eslintrc.yaml" -o -f ".eslintrc.yml" -o -f ".eslintrc.json" -o -f ".eslintrc" ]; then
-  echo -e "${RED}Existing ESLint config file(s) 
+  echo -e "${RED}Existing ESLint config file(s) found
