edbfa5371dde10415698909c062bced092869100
YCtee <104292374+YCtee@users.noreply.github.com>
2020-08-15 15:09:15 +0000

    add some icons

--- eslint-prettier-config.sh
+++ eslint-prettier-config.sh
@@ -58,4 +58,4 @@ finished=false
 
 # Max Line Length
 while ! $finished; do
-  read -p "${LCYAN}What max line length do you want to set 
+  read -p "${LCYAN}What max line length do you want to set for ESLi
