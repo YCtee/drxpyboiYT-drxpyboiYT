9a7801ef7a00978cb7e76e53a3fe0ed7918f42b5
YCtee <104292374+YCtee@users.noreply.github.com>
2020-11-03 16:26:39 +0000

    Minor patch

--- eslint-prettier-config.sh
+++ eslint-prettier-config.sh
@@ -60,4 +60,5 @@ finished=false
 while ! $finished; do
   read -p "${LCYAN}What max line length do you want to set for ESLint and Prettier? (Recommendation: 100) ${NC}"
   if [[ $REPLY =~ ^[0-9]{2,3}$ ]]; then
-    max_len_val=$REP
+    max_len_val=$REPLY
+    fin
