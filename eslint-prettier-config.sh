c4b910f8a26f4af05c49f5e6f1f28244fc220046
YCtee <104292374+YCtee@users.noreply.github.com>
2021-09-18 18:02:59 +0000

    working on physical goals

--- eslint-prettier-config.sh
+++ eslint-prettier-config.sh
@@ -107,4 +107,4 @@ if [ "$skip_eslint_setup" == "true" ]; then
   break
 else
   echo
-  echo -e "4/4 ${YELLOW}Building your .eslintrc${con
+  echo -e "4/4 ${YELLOW}Building your .eslintrc${config_exten
